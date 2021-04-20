fc1083871a27f92f2b92c7a8c39a54d1de42aa69
Daniel Needham <danielneedhamdn@gmail.com>
2021-04-20 21:19:51 +0100

    Added egg/ contact pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import { pageLoad } from "./pageload";
+import { eggsLoad } from "./pages/egg";
+import { homeLoad } from "./pages/home";
 pageLoad();
+homeLoad();
 
 const navbar = document.querySelector("#navbar");
-navbar.addEventListener("click", (e) => { //add event listener to gameboard div
-    if (e.target.id === "Home") { 
-        pageLoad();
+navbar.addEventListener("click", (e) => {
+    console.log(e);
+    const container = document.getElementById("container");
+    const main = document.getElementById("main");
+    if (main) container.removeChild(main);
+
+    if (e.target.id === "home") { 
+        eggsLoad();
         console.log("Is this on")
     }
+
+    if (e.target.id === "eggs") {
+        eggsLoad();
+        console.log("ok")
+    }
+
+    if (e.target.id === "contact") {
+        console.log("contact")
+    }
+
 });
--- src/pages/egg.js
+++ src/pages/egg.js
@@ -1,8 +1,8 @@
-let homeLoad = () => {
+let eggsLoad = () => {
     const container = document.getElementById("container")
 
     const footer = document.getElementById("footer");
-    footer.insertAdjacentHTML("beforebegin", `<div id="main"><h1 class="full">Scotch Eggs as you have never had them before!</h1>
+    footer.insertAdjacentHTML("beforebegin", `<div id="main"><h1 class="full">What Eggs you want fam</h1>
     <p>Here at the Hove Egg Van, we are just crazy about Scotch Eggs. If you never had a fully loaded scotch egg then
       you are seriously missing out.
       Check out our location page, to find where you can catch us next!</p>
@@ -12,4 +12,4 @@ let homeLoad = () => {
 
 }
 
-export { homeLoad };
+export { eggsLoad };
